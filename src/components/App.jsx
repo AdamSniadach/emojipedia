329684f0f963bf9b019f6ed91570bfeedd75dc8a
Adam <126939161+AdamSniadach@users.noreply.github.com>
2023-06-18 19:05:28 +0000

    create first component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import emojipedia from "../emojipedia";
 import Card from "./Card";
 
+
+//step 2 create funcion to display cards
 function CardCreate(props) {
   return (
     <Card
@@ -19,7 +21,7 @@ function App(props) {
       <h1>
         <span>emojipedia</span>
       </h1>
-
+//step3 add map to show all cards 
       <dl className="dictionary">{emojipedia.map(CardCreate)};</dl>
     </div>
   );
