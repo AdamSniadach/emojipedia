7b2537335cbb95d6b1594965bf2dfc9d8fd875e4
Adam <126939161+AdamSniadach@users.noreply.github.com>
2023-06-18 20:01:21 +0100

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span>{props.name}</span>
+      </dt>
+      <span className="emoji" role="img" aria-label={props.name}>
+        {props.emoji}
+      </span>
+      <dd>{props.meaning}</dd>
+    </div>
+  );
+}
+
+export default Card;
